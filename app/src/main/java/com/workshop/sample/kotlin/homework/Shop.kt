ff00372ebf5c2305886f3b44a4805e0cfab753fa
samkzm <47472976+samkzm@users.noreply.github.com>
2019-02-11 19:20:40 +0330

    Update Shop.kt

--- app/src/main/java/com/workshop/sample/kotlin/homework/Shop.kt
+++ app/src/main/java/com/workshop/sample/kotlin/homework/Shop.kt
@@ -4,7 +4,14 @@ data class Shop(val name: String, val customers: List<Customer>)
 
 data class Customer(val name: String, val city: City, val orders: List<Order>)
 
-data class Order(val products: List<Product>, val isDelivered: Boolean)
+data class Order(val products: List<Product>, val isDelivered: Boolean){
+    fun totalprice():Double{
+        var sum:Double=0.0
+        for (i in products)
+            sum+=i.price
+        return sum
+    }
+}
 
 data class Product(val name: String, val price: Double)
 
